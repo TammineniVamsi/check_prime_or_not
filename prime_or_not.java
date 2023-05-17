2faea66a8cd01dc0b4501a950ed41989f8828a7e
Tammineni Vamsi <105577595+TammineniVamsi@users.noreply.github.com>
2023-05-17 22:18:43 +0530

    Create prime_or_not.java

---
+++ prime_or_not.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.math.*;
+public class primeornot{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter n value");
+        int n=sc.nextInt();
+
+        boolean isprime=true;
+
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                isprime=false;
+            }
+        }
+        if(isprime==true){
+            System.out.println(n+"is prime");
+        }
+        else{
+            System.out.println(n + " is not prime");
+        }
+    }
+
+}
